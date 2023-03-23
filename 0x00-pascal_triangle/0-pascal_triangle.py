98ff86a3636c890249f326ae232e2b952e426295
joethesaint <joebamisaye068@gmail.com>
2023-03-23 13:42:15 -0400

    push files

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+pascal_triangle
+"""
+
 def pascal_triangle(n):
+    """
+    A function that returns a list of lists of integers
+    representing the Pascal's triangle of n
+    """
     if n <= 0:
         return []
 
