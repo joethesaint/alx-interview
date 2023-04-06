5f75516723edfecc976d543329704e6e7e279ae2
joethesaint <joebamisaye068@gmail.com>
2023-04-05 21:23:55 -0400

    add task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+2. Minimum Operations
+"""
+def minOperations(n):
+    """
+    Calculates the fewest number of operations
+    needed to result in exactly n H characters
+    in the file
+        Returns:
+            Integer - if n is impossible to acheive,
+            return 0
+    """
+    
