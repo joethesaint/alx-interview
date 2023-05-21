04ffcf84083056a81ade79b3c01f96e9bf79ad47
joethesaint <joebamisaye068@gmail.com>
2023-05-21 01:47:53 +0000

    feat: add module

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Making change 0(n)
+"""
+
+
+def makeChange(coins, total):
+    """
+    Classic Bottom-Up dynamic program
+    """
+    temp_val = 0 #temporary value
+    coins.sort(reverse=True)
+
+    if total < 0:
+        return 0
+
+    for coin in coins:
+        if total % coin <= total:
+            temp_val += total // coin
+            total = total % coin
+
+    return temp_val if total == 0 else -1
